594950edefefa63e61eaa5a222be2e358e4c904d
aphiwe-debug <aphiwemagwaza@icloud.com>
2026-09-01 18:24:55 +0200

    created a user model
    Add User class to track user data in chatbot
    
    A new `User` class was added to the `CybersecurityChatbot` namespace.
    This class includes `Name` and `QuestionsAsked` properties to
    represent user information and interaction history.
    
    The `System.Media` namespace was also added, though its purpose
    is not yet clear from the current changes.

--- Cybersecurity Awareness Bot Part 1/Program.cs
+++ Cybersecurity Awareness Bot Part 1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Media;
+
+namespace CybersecurityChatbot
+{
+
+    // MODEL: User
+    public class User
+    {
+        public string Name { get; set; } = string.Empty;
+        public int QuestionsAsked { get; set; } = 0;
+    }
